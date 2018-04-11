76f235b152507d6a8304832435ccfe33a29c7c4b
konglong <anmingzhe@me.com>
2018-04-11 16:41:14 +0800

    [update] 突然想把之前写的这个渣渣东西放npm上 这样我写活动页面就可以直接npm安装了，6不6?

---
+++ README.md
@@ -0,0 +1,34 @@
+## jsonp
+
+*   超级轻量jsonp
+
+## demo
+
+```javascript
+git clone https://github.com/webkonglong/jsonp.git
+
+然后打开index.html 打开浏览器控制台 -> Network -> js或all 查看
+
+```
+
+## 说明
+
+* 接口肯定是以 ```/jsonp``` 结尾, 但请求接口不需要写 ```/jsonp``` 这个库会帮你自动加上
+* 只需要 ```var ajax = new Jsonp({
+        api: 'http://www.baidu.com/'
+      });```
+* 一次即可。后面所有的都是 ```ajax.get(url, data, successCallback)``` 这种用法
+* ```url``` 请求参数 完整的请求链接为 ```http://www.baidu.com/api/getname/jsonp``` 因为你在 ```new Jsonp({ api: 'http://www.baidu.com/' })``` 所以 ```url``` 填写 ```api/getname``` 即可。
+* ```new Jsonp ``` 的 ```api``` 会添加在你所有请求的url前面，可以不填写。
+
+
+
+🎉 觉得好用给一个 star 哦~ 🎉
+
+
+
+
+
+## Other
+
+*   如果其他问题可以邮箱沟通，anmingzhe@me.com;
---
+++ jsonp.js
@@ -0,0 +1,62 @@
+
+function Jsonp (option) {
+  this.option = option;
+  this.counter = 0;
+  this.head = null;
+  this.query = null;
+}
+
+Jsonp.prototype.get = function (url, params, callback) {
+  this.query = (url || '').indexOf('?') === -1 ? '?' : '&';
+
+  this.connectUrl(params);
+
+  var uniqueName = "callback_json" + (++this.counter);
+
+  this.setCallback(callback, uniqueName);
+
+  var api = this.option && this.option.api && this.option.api || '';
+
+  this.load(api + url + '/jsonp' + this.query + 'callback' + '=' + uniqueName);
+
+  return uniqueName;
+}
+
+Jsonp.prototype.connectUrl = function (params) {
+  for (key in params) {
+    if (params.hasOwnProperty(key)) {
+      this.query += encodeURIComponent(key) + "=" + encodeURIComponent(params[key]) + "&";
+    }
+  }
+}
+
+Jsonp.prototype.setCallback = function(callback, callbackName) {
+  window[callbackName] = function(data) {
+    callback(data);
+    try {
+      window[callbackName] = null;
+      delete window[callbackName];
+    } catch (e) {}
+  };
+};
+
+Jsonp.prototype.load = function (url) {
+  var script = document.createElement('script'),
+    done = false;
+    script.src = url;
+    script.async = true;
+  script.onload = script.onreadystatechange = function () {
+    if (!done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete")) {
+      done = true;
+      script.onload = script.onreadystatechange = null;
+      if (script && script.parentNode) {
+        script.parentNode.removeChild(script);
+      }
+    }
+  };
+
+  if (!this.head) {
+    this.head = document.getElementsByTagName('head')[0];
+  }
+  this.head.appendChild(script);
+}
